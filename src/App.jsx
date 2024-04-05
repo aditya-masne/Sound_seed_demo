dbd629806b2dbafc33c75d4e977abe62911a868e
aditya-masne <adityamasne33@gmail.com>
2024-04-05 17:58:32 +0530

    added project1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar/Navbar.jsx'
+import React from 'react';
+
+const App = () => {
+
+  return (
+    <>
+    <Navbar />
+    </>
+  );
+};
+
+export default App;
